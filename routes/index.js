8bb08b1e768fce24184ee961c88da873de056524
dmitriyvelichkodev <dmitriy.velichko@pipedrive.com>
2018-04-22 12:07:20 +0300

    inital commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var keystone = require('keystone');
+var middleware = require('./middleware');
+var importRoutes = keystone.importer(__dirname);
+
+// Common Middleware
+keystone.pre('routes', middleware.initLocals);
+keystone.pre('render', middleware.flashMessages);
+
+// Import Route Controllers
+var routes = {
+	views: importRoutes('./views'),
+};
+
+// Setup Route Bindings
+exports = module.exports = function (app) {
+	// Views
+	app.get('/', routes.views.index);
+	app.get('/gallery', routes.views.gallery);
+	app.all('/contact', routes.views.contact);
+
+	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+	// app.get('/protected', middleware.requireUser, routes.views.protected);
+
+};
